2f39337bd3bdaac3b75b1c9b884ce6ffd4276916
Tad Martin <tadfmartin@gmail.com>
2024-03-10 11:59:02 -0400

    updated to use file-loader in webpack

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,4 @@
-import berriesImage from './noun-berries-2484585.svg';
+// import berriesImage from './noun-berries-2484585.svg';
 
 const menuPage = function() {
     
@@ -16,8 +16,8 @@ const menuPage = function() {
     // appetizers
     const apps_left = document.createElement('div');
     const apps_left_img = document.createElement('img');
-    // apps_left_img.src = 'noun-berries-2484585.svg';
-    apps_left_img.src = berriesImage;
+    apps_left_img.src = 'noun-berries-2484585.svg';
+    // apps_left_img.src = berriesImage;
     apps_left_img.width = 50;
     apps_left.appendChild(apps_left_img);
     apps_left.classList.add('menu_item', 'title_left');
